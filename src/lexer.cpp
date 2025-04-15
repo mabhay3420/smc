c6d644766395f95f171fc606d1a83a9b61694fb9
Abhay Mishra <mishraabhay9179913420@gmail.com>
2025-04-15 18:45:03 +0530

    initial cmake setup

---
+++ src/lexer.cpp
@@ -0,0 +1,6 @@
+#include "lexer.hpp"
+
+void test() {
+    int a = 0;
+    std::cout << "test" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "lexer.hpp"
+
+int main() {
+    test();
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
